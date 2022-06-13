7e1be42694eb484e4df4affeea4d071493baf654
misha <20xim04@gmail.com>
2022-06-13 14:42:25 +0300

    Add Restart Program
    WORK ON LINUX

---
+++ restart/restart.cpp
@@ -0,0 +1,146 @@
+#include"restart.h"
+void ReverChar(const char* arr,char* out){
+   
+
+    int len=strlen(arr);
+    for(int i=0;i<len;i++){
+        out[i]=arr[len-i-1];
+    }
+    out[len]='\0';
+    
+}
+std::string GetStrinG(char *arr, int startindex, int size)
+{
+    char *text = (char *)malloc(size+1);
+    memcpy(text, arr + startindex, size);
+    text[size]='\0';
+    std::string t=std::move(text);
+    return t;
+}
+int SearchWordIndex(char *arr, int startindex, int size, int count, const char *search)
+{
+    int len = strlen(search);
+    int c = 0;
+    
+    if (startindex > size)
+    {
+        char s[100];
+        ReverChar(search,s);
+        for (int i = startindex; i >= size; i--)
+        {
+            for (int j = i; j >= size; j--)
+            {
+                if (arr[j] == s[i-j])
+                {
+                    if (i-j == len - 1)
+                    {
+                        c++;
+                        if (c == count)
+                            return i;
+                    }
+                }
+                else
+                {
+                    i -= i-j;
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int i = startindex; i < size; i++)
+        {
+            for (int j = i; j < size; j++)
+            {
+                if (arr[j] == search[j - i])
+                {
+                    if (j - i == len - 1)
+                    {
+                        c++;
+                        if (c == count)
+                            return i;
+                    }
+                }
+                else
+                {
+                    i += j - i;
+                    break;
+                }
+            }
+        }
+        return -1;
+    }
+}
+void StartProgram(){
+    system("./AZS");
+}
+
+std::string GetPwdProc(const char* path){
+   
+    std::ifstream f;
+    std::string read;
+    f.open(path,std::ios_base::in);
+    if(f.is_open()){
+       
+        f>>read;
+        f.close();
+    }
+    return read;
+}
+void ShowPid(PID* pid){
+ std::cout<<"PID: "<<pid->pid<<" PWD: "<<pid->pwd<<" NAME: "<<pid->nameprogram<<" PWD DIR: "<<pid->pwddir<<"\n";
+}
+std::vector<PID> FindProc(){
+    char mypwd[300];
+    getcwd(mypwd,300);
+    DIR *dir; struct dirent *diread;
+    vector<PID> pids;
+     std::vector<PID> find;
+    if ((dir = opendir("/proc")) != nullptr) {
+        while ((diread = readdir(dir)) != nullptr) {
+            PID pid;
+            
+            if(atoi(diread->d_name)!=0){
+                pid.pid=atoi(diread->d_name);
+                pids.push_back(pid);
+            }
+        }
+        closedir (dir);
+    } else {
+        perror ("opendir");
+        return find;
+    }
+
+    for(int i=0;i<pids.size();i++){
+       
+        std::string path="/proc/"+std::to_string(pids[i].pid)+"/cmdline";
+        std::string pwd= GetPwdProc(path.c_str());
+        pids[i].pwd=pwd;
+        int startname=SearchWordIndex(&pwd[0],pwd.length(),0,1,"/");
+        if(startname!=-1){
+            pids[i].nameprogram=GetStrinG(&pwd[startname+1],0,pwd.length()-startname-1);
+            pids[i].pwddir=GetStrinG(&pwd[0],0,startname);
+        }
+        //ShowPid(&pids[i]);
+        
+    }
+   
+    
+    for(int i=0;i<pids.size();i++){
+        if(pids[i].nameprogram=="AZS"){
+            find.push_back(pids[i]);
+            
+        }
+    }
+    std::cout<<"MY PWD: "<<mypwd<<"\n";
+    return find;
+}
+int main(){
+    std::vector<PID> azs=FindProc();
+    for(int i=0;i<azs.size();i++){
+        kill(azs[i].pid,9);
+    }
+    StartProgram();
+    return 0;
+}
---
+++ restart/restart.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <dirent.h>
+#include <unistd.h>
+#include<string.h>
+#include <fstream>
+#include <sys/types.h>
+#include <signal.h>
+using std::cout; using std::cin;
+using std::endl; using std::vector;
+struct PID{
+    int pid;
+    std::string pwddir;
+    std::string nameprogram;
+    std::string pwd;
+};
+void ShowPid(PID* pid);
+std::string GetStringG(char* arr,int startindex,int size);
+void ReverChar(const char* arr,char* out);
+int SearchWordIndex(char* arr,int startindex,int size,int count,const char* search);
+void StartProgram();
+std::vector<PID> FindProc();
+std::string GetPwdProc(const char* path);
+int main();
